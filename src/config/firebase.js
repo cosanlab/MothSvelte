eb02830674727b8ab3fff58b4c12c3bcd25cde67
Hikmat-Bangash <hikmatkhanbanagash@gmail.com>
2023-08-01 15:56:43 +0500

    Done with firebase integration, data storage, and code update for deployment.

---
+++ src/config/firebase.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+
+import {
+  PUBLIC_API_KEY,
+  PUBLIC_AUTH_DOMAIN,
+  PUBLIC_PROJECT_ID,
+  PUBLIC_STORAGE_BUCKET,
+  PUBLIC_MESSAGING_SENDERID,
+  PUBLIC_APP_ID,
+  PUBLIC_MEASUREMENT_ID,
+} from "$env/static/public";
+
+
+const firebaseConfig = {
+  apiKey: PUBLIC_API_KEY,
+  authDomain: PUBLIC_AUTH_DOMAIN,
+  projectId: PUBLIC_PROJECT_ID,
+  storageBucket: PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: PUBLIC_MESSAGING_SENDERID,
+  appId: PUBLIC_APP_ID,
+  measurementId: PUBLIC_MEASUREMENT_ID,
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
